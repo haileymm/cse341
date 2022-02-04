c977ba6d28f34cc29479db5b8a7077212d373375
haileymm <71346336+haileymm@users.noreply.github.com>
2022-02-04 14:38:44 -0800

    week05

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 const path = require('path');
 
-const cors = require('cors');// Place this with other requires (like 'path' and 'express')
-const PORT = process.env.PORT || 5000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://haileymg:Hmmajm987!21@cluster0.jvprn.mongodb.net/shop';
+
 const app = express();
-
-const corsOptions = {
-  origin: "https://cse341mundt.herokuapp.com/",
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
-
-const options = {
-  family: 4
-};
-
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://haileymg:Hmmajm987!21@cluster0.jvprn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-                      
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -42,23 +53,23 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGODB_URL, options
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    // This should be your user handling code implement following the course videos
-    app.listen(PORT);
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
   });
-
-  
